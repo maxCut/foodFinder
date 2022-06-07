29722cda56497795f79380e19278c4c71a8bbd0e
Max Mesirow <max.mesirow@gmail.com>
2022-06-07 00:44:14 -0400

    designed beginings of site

--- chrome-extension/content-script.js
+++ chrome-extension/content-script.js
@@ -1,5 +1,15 @@
 console.log("here");
-document.addEventListener("hello", function(data)
+document.addEventListener("set", function(event)
 {
-    alert("got message");
+    console.log(event.detail)
+    chrome.storage.local.set({"message":event.detail},function(){})
+});
+
+document.addEventListener("get", function(event)
+{
+    console.log(event.detail)
+    chrome.storage.local.get(["message"],function(result){
+        console.log("got data")
+        console.log(result.message)
+    })
 });
