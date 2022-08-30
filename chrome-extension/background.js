b049a536b02f6cb12d23cffead494d5deac4569e
Max Mesirow <max.mesirow@gmail.com>
2022-08-30 19:19:21 -0400

    made initial version

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,20 +1,111 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('onInstalled...');
-  alert("Hello, world!");
-  // create alarm after extension is installed / upgraded
-  chrome.alarms.create('refresh', { periodInMinutes: 3 });
+    console.log("onInstalled...");
 });
-
 
 window.perfWatch = {};
-
-chrome.runtime.onMessage.addListener(
-    (message,sender,sendResponse)=> 
-    {
-        console.log("got message");
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.type == "foodFinderMessage") {
+        if (request.subtype == "ASIN_addToCart_request") {
+            addItemsToFreshCart(request.ASIN_set).then;
+            {
+                sendResponse("success");
+                return;
+            }
+        }
     }
-);
 
-chrome.alarms.onAlarm.addListener((alarm) => {
-  alert("Hello, world!");
+    sendResponse("unknown request");
 });
+
+async function checkIfAmazonLoggedIn() {
+    storeUrl = "https://www.amazon.com";
+
+    var signOn;
+    await fetch(`${storeUrl}/alm/storefront?almBrandId=QW1hem9uIEZyZXNo`)
+        .then((response) => {
+            return response.text();
+        })
+        .then((html) => {
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(html, "text/html");
+            signOn = doc.getElementById("nav-link-accountList");
+        })
+        .catch((err) => {});
+
+    if (!signOn) {
+        return false;
+    }
+    return signOn.data-nav-ref != "nav_ya_signin";
+}
+
+async function fetchOffer(element, storeUrl) {
+    return await fetch(
+        `${storeUrl}/gp/product/${element.asin}?almBrandId=QW1hem9uIEZyZXNo&fpw=alm&linkCode=ll1&tag=foodfinder00-20`
+    )
+        .then((response) => {
+            return response.text();
+        })
+        .then((html) => {
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(html, "text/html");
+            let els = doc.querySelectorAll("[data-fresh-add-to-cart]");
+
+            for (let i = 0; i < els.length; i++) {
+                let el = els[i];
+                let a2c = JSON.parse(el.dataset["freshAddToCart"]);
+
+                if (a2c.asin == element.asin) {
+                    token = a2c.csrfToken;
+                    offer = a2c.offerListingID;
+                    return [offer, token];
+                }
+            }
+        });
+}
+
+async function addItemsToFreshCart(asin_set) {
+    if (!(await checkIfAmazonLoggedIn())) {
+        chrome.tabs.query(
+            { active: true, currentWindow: true },
+            function (tabs) {
+                chrome.tabs.sendMessage(
+                    tabs[0].id,
+                    "notifyLoadCartFailed",
+                    function (response) {}
+                );
+            }
+        );
+        return;
+    }
+    storeUrl = "https://www.amazon.com";
+    addAPI = "https://www.amazon.com/alm/addtofreshcart";
+
+    for (const element of asin_set) {
+        [offer, token] = await fetchOffer(element, storeUrl);
+
+        let body = {
+            asin: element.asin,
+            brandId: "QW1hem9uIEZyZXNo",
+            clientID: "fresh-nereid",
+            offerListingID: offer,
+            quantity: element.quantity,
+            csrfToken: token,
+        };
+
+        await fetch(addAPI, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify(body),
+        });
+    }
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            "notifyLoadCartCompleted",
+            function (response) {}
+        );
+    });
+}
--- chrome-extension/content-script.js
+++ chrome-extension/content-script.js
@@ -1,15 +1,34 @@
-console.log("here");
-document.addEventListener("set", function(event)
-{
-    console.log(event.detail)
-    chrome.storage.local.set({"message":event.detail},function(){})
+console.log("content script");
+document.addEventListener("purchaseRequest", function (event) {
+    console.log(event.detail);
+    //chrome.storage.local.set({ "message": event.detail }, function () { })
+    sendMessageToBackground(event.detail);
 });
 
-document.addEventListener("get", function(event)
-{
-    console.log(event.detail)
-    chrome.storage.local.get(["message"],function(result){
-        console.log("got data")
-        console.log(result.message)
-    })
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    console.log(request);
+    if (request == "notifyLoadCartCompleted") {
+        var event = new CustomEvent("purchaseRequestSuccess", {
+            detail: "success",
+        });
+        document.dispatchEvent(event);
+    }
+    if (request == "notifyLoadCartFailed") {
+        var event = new CustomEvent("purchaseRequestFailed", {
+            detail: "fail",
+        });
+        document.dispatchEvent(event);
+    }
 });
+
+function sendMessageToBackground(cart) {
+    //chrome.runtime.sendMessage({type:"foodFinderMessage",subtype:"ASIN_addToCart_request",ASIN_set:[{asin:"B079M54HW4", quantity:1}]},() => {
+    chrome.runtime.sendMessage(
+        {
+            type: "foodFinderMessage",
+            subtype: "ASIN_addToCart_request",
+            ASIN_set: cart,
+        },
+        (response) => {}
+    );
+}
