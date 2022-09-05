78f6dae4a69fefd777a1c47f94c97af280e2a479
Max Mesirow <max.mesirow@gmail.com>
2022-09-04 23:27:55 -0400

    test

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ app.use(express.static(dir));
 app.listen(3000, function () {
     console.log("Listening on http://localhost:3000/");
 });
-
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(`${__dirname}/../client/index.html`));
 });
