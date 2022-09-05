1513a2f23f459c200db6eb59cc28c5bb15d2bf9e
Max Mesirow <max.mesirow@gmail.com>
2022-09-05 00:00:18 -0400

    set to use dynamic port

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 var path = require("path");
 var express = require("express");
 var app = express();
-
+var port = process.env.PORT || 3000;
 var dir = path.join(__dirname, "../client");
 
 app.use(express.static(dir));
 
-app.listen(3000, function () {
-    console.log("Listening on http://localhost:3000/");
+app.listen(port, function () {
+    console.log("Listening on http://localhost:" + port + "/");
 });
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(`${__dirname}/../client/index.html`));
