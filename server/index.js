bf7abb2de64fc3563b17257183c125b2faacd086
Max Mesirow <max.mesirow@gmail.com>
2022-09-04 23:25:18 -0400

    added node

--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3000;
-const path = require("path");
+var path = require("path");
+var express = require("express");
+var app = express();
 
-const app = express();
+var dir = path.join(__dirname, "../client");
+
+app.use(express.static(dir));
+
+app.listen(3000, function () {
+    console.log("Listening on http://localhost:3000/");
+});
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(`${__dirname}/../client/index.html`));
 });
-
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-});
-
-var http = require("http");
