93a20b507a3649e74e18ca5a05dbc2489932562f
Max Mesirow <max.mesirow@gmail.com>
2022-09-04 22:56:53 -0400

    cleaned

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+
+const app = express();
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(`${__dirname}/../client/index.html`));
+});
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
+
+var http = require("http");
