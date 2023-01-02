30a1c0a6eaa6ff827800f6f0ae2a1a3ebaa36f9c
Max Mesirow <max.mesirow@gmail.com>
2023-01-02 12:45:59 -0500

    fixed shared server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 var path = require("path");
 var express = require("express");
+const { allowedNodeEnvironmentFlags } = require("process");
 var app = express();
 var port = process.env.PORT || 3000;
 var dir = path.join(__dirname, "../client");
 
+app.use("/shared", express.static(path.join(__dirname, "../shared")));
 app.use(express.static(dir));
 
 app.listen(port, function () {
     console.log("Listening on http://localhost:" + port + "/");
 });
+/*
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(`${__dirname}/../client/index.html`));
 });
+*/
